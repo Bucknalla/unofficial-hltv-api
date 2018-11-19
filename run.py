bf81ebfa011917dae5a62a554bc7a53c9a72c87e
Alex Bucknall <alex.bucknall@gmail.com>
2018-11-19 17:16:04 +0000

    tweaked example

--- run.py
+++ run.py
@@ -1,17 +1,19 @@
 from hltv import hltv
 import pprint
 
-# h = hltv.hltv()
-
-for matches in hltv.hltv.upcomingMatches():
-    pprint.pprint(matches)
 
 h = hltv.match("2329057")
 
-for odd in h.odds():
-    print(type(odd[0]))
-    # print(odd[0])
-    if str(odd[0]) == 'betway':
-        print('adassda')
+i = hltv.hltv()
 
-print(h.maps())
+pprint.pprint(i.upcomingMatches())
+
+
+
+# for odd in h.odds():
+#     print(type(odd[0]))
+#     # print(odd[0])
+#     if str(odd[0]) == 'betway':
+#         print('adassda')
+
+# print(h.maps())
